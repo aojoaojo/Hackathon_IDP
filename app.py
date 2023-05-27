5440425893af59784da8195614580f680b4f033d
Aojoaojo <aojoaojov@gmail.com>
2023-05-26 23:49:12 -0300

    Mudanças nos nomes do menu e variáveis. Mudança nas fotos do cardápio.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ Equipe: João Vitor Brandão, Rafael Nogueira, Lucas Rocha, João Victor Távora
 '''
 
 import flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 
 import requests
 
@@ -40,41 +40,45 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template('index_inicial.html')
+
+@app.route("/cardapio")
+def cardapio():
     name = request.args.get("name")
     url = 'https://hackarestaurante-os-conquistadores-da-disrupcao.azurewebsites.net'
     caminho = '/api/cliente/categorias'
+    r = requests.get(url+caminho)
+    
+    resposta = r.json()
+    print(resposta)
+    if request.method == 'POST':
+         return render_template('cardapio.html',name=name,resposta=resposta,categoria_post=request.form['categoria'])
+    return render_template('cardapio.html',name=name,resposta=resposta)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    name = request.args.get("name")
+    url = 'https://hackarestaurante-os-conquistadores-da-disrupcao.azurewebsites.net'
+    caminho = '/api/cliente/usuarios'
     r = requests.get(url+caminho)
     resposta = r.json()
-    if request.method == 'POST':
-        return render_template('index.html',name=name,resposta=resposta,categoria_post=request.form['categoria'])
-    return render_template('index.html',name=name,resposta=resposta)
-
-@app.route("/sobre")
-def sobre():
-    return render_template('sobre.html')
+    
+    name = request.form['username']
+    passw = request.form['password']
+    try:
+        data = User.query.filter_by(username=name, password=passw).first()
+        if data is not None:
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+        else:
+            return 'Erro login - usuário não encontrado'
+    except:
+        return "Erro Login"
 
 @app.route("/gosto")
 def gosto():
     return render_template('gosto.html',data=data,cientist_data=cientist_data,netflix_data=netflix_data)
 
-@app.route("/links")
-def links():
-    return render_template('links.html')
-
-@app.route("/teste")
-def teste():
-    return render_template('teste.html')
-
-@app.route("/recursos")
-def recursos():
-    return render_template('recursos.html')
-
-@app.route("/pessoal")
-def pessoal():
-    return render_template('pessoal.html')
-
-
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=True)
